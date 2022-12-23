7f0d4fb2f5abc306c1e069f808eac54a2704645e
Federico Santoro <federicogabsantoro@gmail.com>
2022-12-23 10:56:53 -0300

    Pokedex

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const wantedPokemon = document.getElementById("pokemonId"),
-  searchButton = document.getElementById("search"),
-  formulario = document.getElementById("form");
+const wantedPokemonId = document.getElementById("pokemonId");
+const wantedPokemonName = document.getElementById("pokemonName");
+const formularioId = document.getElementById("formId");
+const formularioName = document.getElementById("formName");
 const cardContainer = document.getElementById("cards-container");
 
-async function getPokemon() {
+async function getPokemonId(id) {
   try {
-    const response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${wantedPokemon.value}`
-    );
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
     let data = await response.json();
     return data;
   } catch (e) {
@@ -15,24 +14,23 @@ async function getPokemon() {
   }
 }
 
-async function searchPokemon(e) {
+async function searchPokemonId(e) {
   try {
-    let tipos;
     e.preventDefault();
-    const pokemon = await getPokemon();
+    const pokemon = await getPokemonId(wantedPokemonId.value);
     console.log(pokemon);
     cardContainer.innerHTML = `
             <div class="card-container">
                 <img class="foto" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-                  wantedPokemon.value
+                  wantedPokemonId.value
                 }.png" />
                   <h2 class="nombre">${pokemon.name}</h2>
                       <div class="tipos">
-                          ${(tipos = pokemon.types
+                          ${pokemon.types
                             .map((element) => {
                               return `<h3 class="tipo ${element.type.name}">${element.type.name}</h3>`;
                             })
-                            .join(""))}
+                            .join("")}
                       </div>
                   <div class="altura-peso">
                       <h3 class="altura">Altura: ${pokemon.height / 10}mts</h3>
@@ -40,23 +38,63 @@ async function searchPokemon(e) {
                   </div>
             </div>
         `;
-        formulario.reset();
+    formularioId.reset();
   } catch (e) {
-    if (wantedPokemon.value.length === 0) {
+    if (wantedPokemonId.value.length === 0) {
       cardContainer.innerHTML = `
-        <h2 class="error"> Ingrese un ID de pokemon por favor </h2>
+        <h2 class="error"> Ingrese un Nombre o ID de pokemon por favor </h2>
         `;
     } else {
       cardContainer.innerHTML = `
         <h2 class="error"> Pokemon no Encontrado </h2>
         `;
     }
-    formulario.reset();
+    formularioId.reset();
+  }
+}
+
+async function searchPokemonName(e) {
+  try {
+    e.preventDefault();
+    const pokemon = await getPokemonId(wantedPokemonName.value);
+    console.log(pokemon);
+    cardContainer.innerHTML = `
+            <div class="card-container">
+                <img class="foto" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                  pokemon.id
+                }.png" />
+                  <h2 class="nombre">${pokemon.name}</h2>
+                      <div class="tipos">
+                          ${pokemon.types
+                            .map((element) => {
+                              return `<h3 class="tipo ${element.type.name}">${element.type.name}</h3>`;
+                            })
+                            .join("")}
+                      </div>
+                  <div class="altura-peso">
+                      <h3 class="altura">Altura: ${pokemon.height / 10}mts</h3>
+                      <h3 class="peso">Peso: ${pokemon.weight / 10}kg</h3>
+                  </div>
+            </div>
+        `;
+    formularioName.reset();
+  } catch (e) {
+    if (wantedPokemonName.value.length === 0) {
+      cardContainer.innerHTML = `
+        <h2 class="error"> Ingrese un Nombre o ID de pokemon por favor </h2>
+        `;
+    } else {
+      cardContainer.innerHTML = `
+        <h2 class="error"> Pokemon no Encontrado </h2>
+        `;
+    }
+    formularioName.reset();
   }
 }
 
 function init() {
-  formulario.addEventListener("submit", searchPokemon);
+  formularioId.addEventListener("submit", searchPokemonId);
+  formularioName.addEventListener("submit", searchPokemonName);
 }
 
 init();
